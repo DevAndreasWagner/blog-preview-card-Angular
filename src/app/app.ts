95220e8789bcd505d7e9b033f9a778169e208cca
Andreas Wagner <kontakt@andreaswagner-amheuberg.de>
2026-09-24 21:29:59 +0200

    finished card-design

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Card } from './card/card';
 
 @Component({
-  imports: [RouterOutlet, Card],
+  imports: [Card],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
